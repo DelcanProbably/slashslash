fe256fd1c40287cb236888887f88bab1b7f9d864
delcanprobably <decbot100@gmail.com>
2026-01-12 01:51:22 +1100

    Updated contact page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready( () => {
     // dynamically generate email address at runtime to avoid those bastard scrapers
-    var mail = $('a.email');
-    var href = mail.attr('href').replace('.', '@gmail.com');
-    var cont = mail.text().replace(' at ', '@');
-    mail.attr('href', href);
-    mail.text(cont);
-    console.log("replaced href with " + href);
+    $('a.email').each( (index, element) => {
+        var mail = $(element);
+        var href = mail.attr('href').replace('.', '@gmail.com');
+        var cont = mail.html().replace('_', '@gmail.com');
+        mail.attr('href', href);
+        mail.html(cont);
+        console.log("replaced href with " + href);
+    });
 });
